e4e5ff0e5197f076d65f181b2a7da18f06fb58fc
aghara urvashee <110652436+urvashee20@users.noreply.github.com>
2026-03-23 18:54:08 +0530

    add FE project run command in Program.cs file

--- SmartHealthCompanion/Program.cs
+++ SmartHealthCompanion/Program.cs
@@ -35,6 +35,11 @@ builder.Services.AddScoped<JwtService>();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddHttpClient<GeminiService>();
 builder.Services.AddScoped<AIPlanService>();
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll", policy => {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -74,6 +79,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();

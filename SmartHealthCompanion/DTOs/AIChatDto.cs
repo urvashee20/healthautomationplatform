5bd14aa53673f3dd2641c99cdc4c8036b2730d6a
aghara urvashee <110652436+urvashee20@users.noreply.github.com>
2026-03-31 17:07:24 +0530

    migration file

---
+++ SmartHealthCompanion/DTOs/AIChatDto.cs
@@ -0,0 +1,23 @@
+﻿namespace SmartHealthCompanion.DTOs
+{
+    public class AIChatDto
+    {
+    }
+    public class SendMessageDto
+    {
+        public long ChatSessionId { get; set; }
+        public string Message { get; set; }
+    }
+    public class ChatResponseDto
+    {
+        public string UserMessage { get; set; }
+        public string AIResponse { get; set; }
+        public double ResponseTimeMs { get; set; }
+    }
+    public class ChatSessionDto
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public DateTime? LastMessageAt { get; set; }
+    }
+}
---
+++ SmartHealthCompanion/Migrations/20260331072239_NewAIChatEntityADD.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartHealthCompanion.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewAIChatEntityADD : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChatSession_AIPlan_AIPlanId",
+                table: "ChatSession");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ChatSession_AIPlanId",
+                table: "ChatSession");
+
+            migrationBuilder.DropColumn(
+                name: "AIPlanId",
+                table: "ChatSession");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "AIPlanId",
+                table: "ChatSession",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatSession_AIPlanId",
+                table: "ChatSession",
+                column: "AIPlanId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChatSession_AIPlan_AIPlanId",
+                table: "ChatSession",
+                column: "AIPlanId",
+                principalTable: "AIPlan",
+                principalColumn: "Id");
+        }
+    }
+}
